bf6bd5487b0e9c8bf4acb296690dd2c23885619e
Alessandro0003 <alessandroquinta3@gmail.com>
2024-02-15 22:53:55 -0300

    🚀 feat: creating application setup

---
+++ drinks-track/prisma/migrations/20240216013847_create_drinks_table/migration.sql
@@ -0,0 +1,6 @@
+-- CreateTable
+CREATE TABLE "Drinks" (
+    "id" TEXT NOT NULL,
+
+    CONSTRAINT "Drinks_pkey" PRIMARY KEY ("id")
+);
---
+++ web/src/pages/app/index.tsx
@@ -0,0 +1,19 @@
+import { withPageAuthRequired } from "@auth0/nextjs-auth0"
+import { useUser } from "@auth0/nextjs-auth0/client"
+
+export default function HomePage() {
+    const { user } = useUser()
+
+    return (
+        <div>
+            <h1>Page Home</h1>
+            <pre>
+                {JSON.stringify(user, null, 2)}
+            </pre>
+
+            <a href="/api/auth/logout">Logout</a>
+        </div>
+    )
+}
+
+export const getServerSideProps = withPageAuthRequired()
