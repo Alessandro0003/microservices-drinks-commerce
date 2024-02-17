70870ba2fe3c22019d4e573e4007d1793c677192
Alessandro0003 <alessandroquinta3@gmail.com>
2024-02-17 03:15:36 -0300

    🚀 feat: create is method to responsible by user authentication

--- drinks-track/src/http/http.module.ts
+++ drinks-track/src/http/http.module.ts
@@ -1,10 +1,15 @@
 import { Module } from '@nestjs/common'
 import { ConfigModule } from '@nestjs/config'
-import { TestResolver } from './test.resolver'
 import { DatabaseModule } from '../database/database.module'
 import { GraphQLModule } from '@nestjs/graphql'
 import path from 'node:path'
 import { ApolloDriver } from '@nestjs/apollo'
+import { DrinksResolver } from './graphql/resolvers/drinks.resolver'
+import { CategoryResolver } from './graphql/resolvers/category.resolver'
+import { PurchasesEndResolver } from './graphql/resolvers/purchases-end.resolver'
+import { DrinksService } from '../services/drinks.service'
+import { CategoryService } from '../services/category.service'
+import { PurchasesEndService } from '../services/purchases-end.service'
 
 @Module({
     imports: [
@@ -15,6 +20,16 @@ import { ApolloDriver } from '@nestjs/apollo'
             autoSchemaFile: path.resolve(process.cwd(), 'src/schema.gql')
         })
     ],
-    providers: [TestResolver]
+    providers: [
+        // Resolvers
+        DrinksResolver,
+        CategoryResolver,
+        PurchasesEndResolver,
+
+        // Services
+        DrinksService,
+        CategoryService,
+        PurchasesEndService
+    ]
 })
 export class HttpModule {}
