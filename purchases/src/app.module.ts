efd925b5e5603f651de9c756e0533ce334c8a99f
Alessandro0003 <alessandroquinta3@gmail.com>
2024-02-24 02:57:40 -0300

    :recycle: refactor: romeve functions a not using

--- purchases/src/app.module.ts
+++ purchases/src/app.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common'
 import { DatabaseModule } from './database/database.module'
 import { HttpModule } from './http/http.module'
 import { ProductsService } from './services/products.service';
+import { MessagingModule } from './messaging/messaging.module';
 
 @Module({
-  imports: [DatabaseModule, HttpModule],
+  imports: [DatabaseModule, HttpModule, MessagingModule],
   controllers: [],
   providers: [ProductsService],
 })
